49c2ee3ba051e0b102c983225365b5d99a798600
Thanh Nhan <hctxnhan@gmail.com>
2023-07-08 14:15:44 +0700

    VH-8: Setup frontend repository

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh', 'import', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'import/no-default-export': 'error',
+    'import/no-duplicates': 'error',
+    indent: ['error', 2],
+  },
+};
