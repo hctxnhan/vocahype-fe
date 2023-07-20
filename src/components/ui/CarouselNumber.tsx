dd964a3b05329e2d390db7d0d9a606e47f5a3af2
Đặng Lê Đăng Khoa <58388376+danglekhoa1912@users.noreply.github.com>
2023-07-20 22:19:45 +0700

    VH 14 (#5)
    * VH-14: Knowledge Page
    
    * update stroke text
    
    * update UI

---
+++ src/components/ui/CarouselNumber.tsx
@@ -0,0 +1,34 @@
+import { color, motion } from 'framer-motion';
+import colors, { white } from 'tailwindcss/colors';
+interface CarouselNumberProps {
+  total: number;
+  current: number;
+}
+
+export function CarouselNumber({ current, total }: CarouselNumberProps) {
+  return (
+    <>
+      {Array.from(Array(total))?.map((_, index) => {
+        const num = ++index;
+        const diff = Math.abs(current - num)
+        return (
+          <motion.div
+            key={num}
+            className="absolute left-0 right-0 flex h-16 items-center justify-center text-center font-medium transition"
+            style={{
+            }}
+            animate={{
+              fontSize: `${100 * (1 - Math.abs(index - current) / 3)}px`,
+              color: current === num ? colors['white'] : colors['white'],
+              bottom: current === num ? '50%' : `${50 + (current - num) * 15}%`,
+              opacity:
+                1 - diff / 3
+            }}
+          >
+            {num}
+          </motion.div>
+        );
+      })}
+    </>
+  );
+}
