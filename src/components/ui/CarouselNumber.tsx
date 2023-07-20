746471f48d7e2eb4e15788bfa8ffff37b455b7e2
danglekhoa1912 <dangkhoa123bn@gmail.com>
2023-07-20 15:50:07 +0700

    update stroke text

--- src/components/ui/CarouselNumber.tsx
+++ src/components/ui/CarouselNumber.tsx
@@ -8,21 +8,22 @@ interface CarouselNumberProps {
 export function CarouselNumber({ current, total }: CarouselNumberProps) {
   return (
     <>
-      {[...Array(total)]?.map((_, num) => {
+      {Array.from(Array(total))?.map((_, index) => {
+        const num = ++index
         return (
           <motion.div
             key={num}
             className="absolute left-0 right-0 text-center text-5xl font-medium"
             animate={{
-              color: current === num + 1 ? '#FFFFFF' : '#ffffff70',
+              color: current === num ? '#FFFFFF' : '#ffffff70',
               bottom:
-                current === num + 1
+                current === num
                   ? '50%'
-                  : `${50 + (current - num - 1) * 10}%`,
-              opacity: Math.abs(current - num - 1) > 2 ? 0 : 1,
+                  : `${50 + (current - num) * 10}%`,
+              opacity: Math.abs(current - num) > 2 ? 0 : 1,
             }}
           >
-            {num + 1}
+            {num}
           </motion.div>
         );
       })}
--- src/pages/KnowledgeCheck/KnowledgeCheck.tsx
+++ src/pages/KnowledgeCheck/KnowledgeCheck.tsx
@@ -41,19 +41,19 @@ export function KnowledgeCheck() {
         <Card className="z-10 mt-16 flex h-fit px-8 py-4">
           <div>
             <h1 className="text-5xl font-black">{currentWords}</h1>
-            <div className="text-lg font-bold text-[#BE123C]/50 ">verb</div>
+            <div className="text-lg font-bold text-rose-500 ">verb</div>
           </div>
           <Button variant={'ghost'} size="icon">
             <SpeakerLoudIcon width={26} height={26} />
           </Button>
         </Card>
-        <div className="text-outline-transparent absolute top-8 font-[tahoma] text-9xl font-bold">
+        <div className="text-outline-transparent absolute top-8 font-display text-9xl font-bold">
           {currentWords}
         </div>
-        <div className="text-outline-transparent absolute right-80 top-44 font-[tahoma] text-9xl font-bold">
+        <div className="text-outline-transparent absolute right-80 top-44 font-display text-9xl font-bold">
           {currentWords}
         </div>
-        <div className="text-outline-transparent absolute left-80 top-72 font-[tahoma] text-9xl font-bold">
+        <div className="text-outline-transparent absolute left-80 top-72 font-display text-9xl font-bold">
           {currentWords}
         </div>
       </div>
@@ -66,7 +66,7 @@ export function KnowledgeCheck() {
           onClick={handleClick}
           variant={'special'}
           size={'xl'}
-          className="min-w-[200px] bg-gradient-to-b from-[#EC3A58] from-0% via-[#EB425A] via-50% to-[#FF737D] to-100% hover:bg-rose-500/80"
+          className="min-w-[200px] bg-gradient-to-b from-rose-500 from-0% via-rose-600 via-50% to-rose-400 to-100% hover:bg-rose-500/80"
         >
           No
         </Button>
@@ -74,7 +74,7 @@ export function KnowledgeCheck() {
           onClick={handleClick}
           variant={'special'}
           size={'xl'}
-          className="ml-8 min-w-[200px] bg-gradient-to-b from-[#06C2B7] from-0% via-[#00B8D1] via-50% to-[#3CCED8] to-100% hover:bg-teal-500/80"
+          className="ml-8 min-w-[200px] bg-gradient-to-b from-cyan-500 from-0% via-cyan-600 via-50% to-cyan-500 to-100% hover:bg-teal-500/80"
         >
           Yes
         </Button>
