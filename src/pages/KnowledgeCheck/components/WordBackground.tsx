5af7c9b472edb1f878fa4d6eb20b634931c24a84
Hoang Cong Thanh Nhan <nhan.hoangcongthanh@ncc.asia>
2023-07-21 10:59:33 +0700

    Refactor WordBackground

--- src/pages/KnowledgeCheck/components/WordBackground.tsx
+++ src/pages/KnowledgeCheck/components/WordBackground.tsx
@@ -4,65 +4,32 @@ interface WordBackgroundProps {
   word: string;
 }
 
+const animate = [
+  [-200, 0],
+  [200, 0],
+  [-400, 100],
+  [400, -100],
+];
+
 export function WordBackground({ word }: WordBackgroundProps) {
   return (
     <div className="center absolute flex-col gap-6">
-      <motion.div
-        initial={{
-          transform: 'translateX(-200px)',
-          opacity: 0,
-        }}
-        animate={{
-          transform: 'translateX(0)',
-          opacity: 1,
-        }}
-        key={word + '1'}
-        className="text-outline-transparent font-display text-9xl font-bold"
-      >
-        {word}
-      </motion.div>
-      <motion.div
-        initial={{
-          transform: 'translateX(200px)',
-          opacity: 0,
-        }}
-        animate={{
-          transform: 'translateX(0)',
-          opacity: 1,
-        }}
-        key={word + '2'}
-        className="text-outline-transparent font-display text-9xl font-bold"
-      >
-        {word}
-      </motion.div>
-      <motion.div
-        initial={{
-          translateX: '-400px',
-          opacity: 0,
-        }}
-        animate={{
-          translateX: '100px',
-          opacity: 1,
-        }}
-        key={word + '3'}
-        className="text-outline-transparent font-display text-9xl font-bold"
-      >
-        {word}
-      </motion.div>
-      <motion.div
-        initial={{
-          translateX: '400px',
-          opacity: 0,
-        }}
-        animate={{
-          translateX: '-100px',
-          opacity: 1,
-        }}
-        key={word + '4'}
-        className="text-outline-transparent transform font-display text-9xl font-bold"
-      >
-        {word}
-      </motion.div>
+      {animate.map(([initial, animate], i) => (
+        <motion.div
+          initial={{
+            translateX: initial,
+            opacity: 0,
+          }}
+          animate={{
+            translateX: animate,
+            opacity: 1,
+          }}
+          key={word + i.toString()}
+          className="text-outline-transparent font-display text-9xl font-bold"
+        >
+          {word}
+        </motion.div>
+      ))}
     </div>
   );
 }
