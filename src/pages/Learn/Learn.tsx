6f93b7af3b3edb01641e3505e9963d924292f2d2
khoadang <dangkhoa123bn@gmail.com>
2023-08-03 00:48:17 +0700

    update ui learn page

--- src/pages/Learn/Learn.tsx
+++ src/pages/Learn/Learn.tsx
@@ -4,14 +4,17 @@ import {
   SpeakerLoudIcon,
   TriangleLeftIcon,
   TriangleRightIcon,
+  ChevronDownIcon,
 } from '@radix-ui/react-icons';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useRef, useState } from 'react';
+import { AnimatePresence, motion, useAnimate } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 import { Example } from './components/Example';
 import { FillParent } from '@/components/layout/FillParent/FillParent';
 
 export function Learn() {
   const [currentMean, setCurrentMean] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [scope, animate] = useAnimate();
   const nextMean = useRef(1);
 
   const listMean = [
@@ -30,10 +33,29 @@ export function Learn() {
     }
   };
 
+  useEffect(() => {
+    animate('.arrow', { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
+    animate(
+      '.card-image',
+      isOpen
+        ? {
+            height: 300,
+          }
+        : {
+            height: 160,
+          }
+    );
+  }, [isOpen]);
+
   return (
-    <div className="relative flex h-full flex-col gap-4">
-      <div className="group relative overflow-hidden rounded-3xl bg-[url('https://static.theprint.in/wp-content/uploads/2022/04/Web_Photo_Editor-71-1024x576.jpg?compress=true')] bg-cover bg-no-repeat px-16 py-8 text-white transition duration-500 hover:h-[400px]">
-        <div className="group-hover:opacity-50 relative z-50">
+    <div ref={scope} className="relative flex h-full flex-col gap-4">
+      <motion.div
+        style={{
+          backgroundImage: `url("https://static.theprint.in/wp-content/uploads/2022/04/Web_Photo_Editor-71-1024x576.jpg?compress=true")`,
+        }}
+        className="card-image relative h-[160] overflow-hidden rounded-3xl bg-cover bg-no-repeat px-16 py-8 text-white transition duration-500"
+      >
+        <div className=" relative z-50">
           <div className="flex items-center gap-4">
             <div className="font-serif text-4xl font-black">SLAUGHTER</div>
             <Button variant={'ghost'} size="icon">
@@ -46,8 +68,14 @@ export function Learn() {
             <QuestionMarkCircledIcon width={18} height={18} />
           </div>
         </div>
-        <FillParent className="z-[9] bg-slate-800/50 transition group-hover:opacity-0" />
-      </div>
+        <FillParent className="z-[9] bg-slate-800/50 transition" />
+        <ChevronDownIcon
+          onClick={() => setIsOpen(!isOpen)}
+          width={30}
+          height={30}
+          className="arrow absolute right-8 z-10"
+        />
+      </motion.div>
       <div className="flex items-center justify-center gap-16 font-display text-sm font-semibold">
         <div
           onClick={handleClick.bind(null, -1)}
@@ -96,18 +124,8 @@ export function Learn() {
           defeated.”"
           word="slaughter"
         />
-        <Example
-          example="“Hardly anyone in the town escaped the slaughter when the rebels were
-          defeated.”"
-          word="slaughter"
-        />
-        <Example
-          example="“Hardly anyone in the town escaped the slaughter when the rebels were
-          defeated.”"
-          word="slaughter"
-        />
       </div>
-      <div className="absolute bottom-0 left-0 right-0 flex justify-between gap-4">
+      <div className="absolute bottom-2 left-0 right-0 flex justify-between gap-4">
         <Button className="w-full rounded-3xl bg-slate-400 px-8 py-6 font-semibold">
           HARD
         </Button>
