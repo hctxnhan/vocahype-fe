ebc803319ef59c858683308229d6e3987d30d39a
khoadang <dangkhoa123bn@gmail.com>
2023-08-04 00:02:29 +0700

    update modal

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,52 +1,117 @@
-import { IconProps } from '@radix-ui/react-icons/dist/types';
 import { Button, ButtonProps } from './button';
 import { Portal } from './portal';
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+import { cn } from '@/lib/utils/utils';
+import { useMemo } from 'react';
+import { Cross1Icon } from '@radix-ui/react-icons';
 
-interface ModalButtonProps extends ButtonProps {
+interface ModalButtonProps
+  extends React.ForwardRefExoticComponent<
+    ButtonProps & React.RefAttributes<HTMLButtonElement>
+  > {
   title?: string;
   hidden?: boolean;
 }
 
 interface ModalProps {
-  open: boolean;
+  isOpen: boolean;
   header?: {
     title: string;
-    class: string;
+    class?: string;
+    showCloseIcon?: boolean;
   };
-  body?: {
-    content: string;
-    class: string;
-    icon: React.ForwardRefExoticComponent<
-      IconProps & React.RefAttributes<SVGSVGElement>
-    >;
-  };
+  children: React.ReactNode;
+  classContent?: string;
+  classContainer?: string;
+  classButtonWrapper?: string;
   saveButton?: ModalButtonProps;
   cancelButton?: ModalButtonProps;
   extraButton?: ModalButtonProps[];
+  handleClose: () => void;
 }
 
 export function Modal(props: ModalProps) {
-  const { open, body, cancelButton, extraButton, header, saveButton } = props;
+  const {
+    isOpen,
+    classContainer,
+    classContent,
+    cancelButton,
+    extraButton,
+    header,
+    saveButton,
+    children,
+    classButtonWrapper,
+    handleClose,
+  } = props;
+
+  const ButtonList = useMemo(() => {
+    const saveBtn = {
+      title: saveButton?.title || 'Continue',
+      hidden: saveButton?.hidden || false,
+      ...saveButton,
+    };
+    const cancelBtn = {
+      title: cancelButton?.title || 'Cancel',
+      hidden: cancelButton?.hidden || false,
+      variant: 'outline',
+      onClick: handleClose,
+      ...cancelButton,
+    };
+    return [
+      cancelBtn,
+      saveBtn,
+      ...(extraButton?.map(button => ({
+        hidden: button?.hidden || true,
+        ...button,
+      })) || []),
+    ];
+  }, [saveButton, cancelButton, extraButton]);
+
+  if (!isOpen) return null;
 
   return (
-    <Portal selector="#root">
-      <div className="absolute left-2/4 top-2/4 w-[700px] -translate-x-2/4 -translate-y-2/4 rounded-[32px] bg-blue-300 px-14 py-8">
-        {/* <div className="global-background absolute inset-0 -z-50" /> */}
-        <div className="text-center text-lg font-bold text-slate-800">
+    <Portal selector="#modal">
+      <div
+        className={cn(
+          'absolute left-2/4 top-2/4 w-[700px] -translate-x-2/4 -translate-y-2/4 rounded-[32px] bg-gradient-to-r from-slate-100 px-14 py-8',
+          classContainer
+        )}
+      >
+        <div className="global-background absolute inset-0 -z-50" />
+        <div
+          className={cn(
+            'text-center text-lg font-bold text-slate-800',
+            header?.class
+          )}
+        >
           Are you sure you want to reset current progression?
+          {header?.showCloseIcon && (
+            <Cross1Icon
+              width={20}
+              height={20}
+              className="absolute right-8 top-4 text-red-500 hover:cursor-pointer"
+              onClick={handleClose}
+            />
+          )}
         </div>
-        <div className="flex gap-8 py-8 text-slate-900">
-          <ExclamationTriangleIcon width={64} height={64} color="red" />
-          <div>
-            Just another confirmation dialog popup library used to confirm user
-            intentions on the web app. Just another confirmation dialog popup
-            library used to confirm user intentions on the web app.
-          </div>
+        <div
+          className={cn(
+            'flex items-center justify-center gap-8 py-8 text-slate-900',
+            classContent
+          )}
+        >
+          {children}
         </div>
-        <div className="flex justify-end gap-3">
-          <Button>Cancel</Button>
-          <Button>Continue</Button>
+        <div className={cn('flex justify-end gap-3', classButtonWrapper)}>
+          {ButtonList.map((button, index) => {
+            const { hidden, title, ...rest } = button;
+            return (
+              !hidden && (
+                <Button {...rest} key={index}>
+                  {title}
+                </Button>
+              )
+            );
+          })}
         </div>
       </div>
     </Portal>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,14 @@
 import { Navbar } from '@/components/layout/Navbar/Navbar';
 import { Sidebar } from '@/components/layout/Sidebar/Sidebar';
 import { Breadcrumb, BreadcrumbItem } from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
+import { Modal } from '@/components/ui/modal';
 import { KnowledgeCheck } from '@/pages/KnowledgeCheck/KnowledgeCheck';
+import { useState } from 'react';
 
 export function MainLayout() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="flex h-screen w-full flex-col px-8 pb-4 text-neutral-950">
       <div className="global-background absolute inset-0 -z-50" />
@@ -17,7 +22,22 @@ export function MainLayout() {
             <BreadcrumbItem>Get Started</BreadcrumbItem>
             <BreadcrumbItem>Knowledge</BreadcrumbItem>
           </Breadcrumb>
-          <KnowledgeCheck />
+          {/* <KnowledgeCheck /> */}
+          <Modal
+            isOpen={isOpen}
+            handleClose={() => {
+              setOpen(false);
+            }}
+          >
+            <div>sas</div>
+          </Modal>
+          <Button
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            as
+          </Button>
         </div>
       </div>
     </div>
