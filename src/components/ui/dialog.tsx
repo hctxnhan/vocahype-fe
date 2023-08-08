19a729b9ec31c477dfc3c68ed3effaae7224502f
danglekhoa1912 <dangkhoa123bn@gmail.com>
2023-08-08 15:58:16 +0700

    VH-42 change dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,128 +1,130 @@
-import { Button, ButtonProps } from './button';
-import { Portal } from './portal';
+import * as React from 'react';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { X } from 'lucide-react';
+
 import { cn } from '@/lib/utils/utils';
-import { useMemo } from 'react';
-import { Cross1Icon } from '@radix-ui/react-icons';
-import { AnimatePresence, motion } from 'framer-motion';
 
-interface ModalButtonProps   {
-  title: string;
-  hidden?: boolean;
-}
+const Dialog = DialogPrimitive.Root;
 
-interface ModalProps {
-  isOpen: boolean;
-  title: string;
-  classTitle?: string;
+const DialogTrigger = DialogPrimitive.Trigger;
+const DialogClose = DialogPrimitive.Close;
+
+const DialogPortal = ({
+  className,
+  ...props
+}: DialogPrimitive.DialogPortalProps) => (
+  <DialogPrimitive.Portal className={cn(className)} {...props} />
+);
+DialogPortal.displayName = DialogPrimitive.Portal.displayName;
+
+export interface DialogContentProps
+  extends React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> {
   showCloseIcon?: boolean;
-  children: React.ReactNode;
-  classContent?: string;
-  classContainer?: string;
-  classButtonWrapper?: string;
-  saveButton?: ModalButtonProps & ButtonProps;
-  cancelButton?: ModalButtonProps & ButtonProps;
-  extraButton?: ModalButtonProps[];
-  handleClose: () => void;
 }
 
-export function Dialog(props: ModalProps) {
-  const {
-    isOpen,
-    classContainer,
-    classContent,
-    cancelButton,
-    extraButton,
-    title,
-    classTitle,
-    showCloseIcon = false,
-    saveButton,
-    children,
-    classButtonWrapper,
-    handleClose,
-  } = props;
+const DialogContent = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  DialogContentProps
+>(({ className, showCloseIcon = false, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 rounded-[32px] bg-gradient-to-br from-blue-100 to-blue-200 px-14 py-8 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:border-neutral-800 dark:bg-neutral-950 md:w-full',
+        className
+      )}
+      {...props}
+    >
+      <div className="global-background absolute inset-0 -z-50" />
+      {children}
+      {showCloseIcon && (
+        <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-neutral-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-neutral-100 data-[state=open]:text-neutral-500 dark:ring-offset-neutral-950 dark:focus:ring-neutral-300 dark:data-[state=open]:bg-neutral-800 dark:data-[state=open]:text-neutral-400">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </DialogPrimitive.Close>
+      )}
+    </DialogPrimitive.Content>
+  </DialogPortal>
+));
+DialogContent.displayName = DialogPrimitive.Content.displayName;
 
-  const ButtonList = useMemo(() => {
-    const saveBtn = {
-      title: saveButton?.title || 'Continue',
-      hidden: saveButton?.hidden || false,
-      ...saveButton,
-    };
-    const cancelBtn = {
-      title: cancelButton?.title || 'Cancel',
-      hidden: cancelButton?.hidden || false,
-      variant: 'outline',
-      onClick: handleClose,
-      ...cancelButton,
-    };
-    return [
-      cancelBtn,
-      saveBtn,
-      ...(extraButton?.map(button => ({
-        hidden: button?.hidden || false,
-        ...button,
-      })) || []),
-    ];
-  }, [saveButton, cancelButton, extraButton]);
+const DialogHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col gap-4 space-y-1.5 text-center sm:text-left',
+      className
+    )}
+    {...props}
+  />
+);
+DialogHeader.displayName = 'DialogHeader';
 
-  const dropIn = {
-    initial: { top: "20%" , opacity: 0, transition: { type: "spring", duration: 0.1, damping: 100, stiffness: 500 } },
-    isOpen: { top: "50%", opacity: 1 },
-    exit: { top: "80%", opacity: 0 }
-  }
+const DialogBody = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex items-center justify-center gap-8 py-8 text-slate-900',
+      className
+    )}
+    {...props}
+  />
+);
+DialogBody.displayName = 'DialogBody';
 
-  return (
-    <Portal selector="body">
-      <AnimatePresence>
-        {isOpen && <motion.div
-          onClick={(e)=>e.stopPropagation()}
-          className={cn(
-            'absolute left-2/4 top-2/4 w-[700px] -translate-x-2/4 -translate-y-2/4 rounded-[32px]  bg-gradient-to-br from-blue-100 to-blue-200 px-14 py-8',
-            classContainer
-          )}
-          variants={dropIn}
-          initial={"initial"}
-          animate={"isOpen"}
-          exit={"exit"}
-        >
-          <div className="global-background absolute inset-0 -z-50" />
-          <div
-            className={cn(
-              'text-center text-lg font-bold text-slate-800',
-              classTitle
-            )}
-          >
-            {title}
-            {showCloseIcon && (
-              <Cross1Icon
-                width={20}
-                height={20}
-                className="absolute right-8 top-4 text-red-500 hover:cursor-pointer"
-                onClick={handleClose}
-              />
-            )}
-          </div>
-          <div
-            className={cn(
-              'flex items-center justify-center gap-8 py-8 text-slate-900',
-              classContent
-            )}
-          >
-            {children}
-          </div>
-          <div className={cn('flex justify-end gap-3', classButtonWrapper)}>
-            {ButtonList.map((button, index) => {
-              const { hidden, title, ...rest } = button;
-              return (
-                !hidden && (
-                  <Button {...rest} key={index}>
-                    {title}
-                  </Button>
-                )
-              );
-            })}
-          </div>
-        </motion.div>}
-      </AnimatePresence>
-    </Portal>
-  );
-}
+const DialogFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className
+    )}
+    {...props}
+  />
+);
+DialogFooter.displayName = 'DialogFooter';
+
+const DialogTitle = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={cn(
+      'text-center text-lg font-semibold leading-none tracking-tight',
+      className
+    )}
+    {...props}
+  />
+));
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+const DialogDescription = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Description
+    ref={ref}
+    className={cn('text-muted-foreground text-sm', className)}
+    {...props}
+  />
+));
+DialogDescription.displayName = DialogPrimitive.Description.displayName;
+
+export {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+  DialogBody,
+  DialogClose,
+};
