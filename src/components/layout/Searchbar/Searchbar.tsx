1b862fea218737ca7a876d53a7a128090e322fa9
danglekhoa1912 <dangkhoa123bn@gmail.com>
2023-08-16 15:31:34 +0700

    VH-51 recent searches

--- src/components/layout/Searchbar/Searchbar.tsx
+++ src/components/layout/Searchbar/Searchbar.tsx
@@ -1,14 +1,24 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { useFocus } from '@/lib/hooks/useFocus';
+import { useClickOutside } from '@/lib/hooks/useClickOutside';
+import {
+  getLocalStorageItem,
+  removeLocalStorageItem,
+  setLocalStorageItem,
+} from '@/lib/utils/localStorage';
 import { cn } from '@/lib/utils/utils';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'wouter';
 import { FillParent } from '../FillParent/FillParent';
 
 export function Searchbar() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const isFocus = useFocus(inputRef);
+  const containerInputRef = useRef<HTMLDivElement>(null);
+  const [history, setHistory] = useState<string[]>([]);
+  const [isFocus, setIsFocus] = useState(false);
+  useClickOutside(containerInputRef, isBlur => {
+    setIsFocus(!isBlur);
+  });
   const [, navigate] = useLocation();
 
   function handleSearch(e: React.FormEvent<HTMLFormElement>) {
@@ -16,11 +26,29 @@ export function Searchbar() {
     const query = inputRef.current?.value;
     if (query) {
       navigate(`/words?search=${query}`);
-      inputRef.current?.blur();
+      const newHistoryList = [...new Set([query, ...history])];
+      setLocalStorageItem('historySearch', newHistoryList);
+      setHistory(newHistoryList);
     }
   }
 
-  const historyList = ['Test', 'Play', 'Hi'];
+  const onRemoveAll = () => {
+    inputRef.current?.focus();
+    removeLocalStorageItem('historySearch');
+    setHistory([]);
+  };
+
+  const onClickWord = (word: string) => {
+    if (inputRef.current) {
+      inputRef.current.value = word;
+      setIsFocus(false);
+      navigate(`/words?search=${word}`);
+    }
+  };
+
+  useEffect(() => {
+    setHistory(getLocalStorageItem<string[]>('historySearch') || []);
+  }, []);
 
   return (
     <>
@@ -31,7 +59,7 @@ export function Searchbar() {
           'z-[9999]': isFocus,
         })}
       >
-        <div className="relative flex-1">
+        <div ref={containerInputRef} className="relative flex-1">
           <Input
             ref={inputRef}
             placeholder="Find definition for..."
@@ -39,19 +67,31 @@ export function Searchbar() {
           />
           {isFocus && (
             <div className="absolute top-12 w-full rounded-lg bg-white">
-              <div className="flex justify-between px-[32px] py-[16px]">
+              <div className="flex items-center justify-between border-b border-solid border-gray-200 px-[32px] py-[16px] text-xs last:border-b-0">
                 <div className="font-semibold text-slate-400">
                   RECENT SEARCH
                 </div>
-                <div className="text-red-700 hover:cursor-pointer">
-                  Remove all
-                </div>
+                {!!history.length && (
+                  <div
+                    onClick={onRemoveAll}
+                    className="text-red-700 hover:cursor-pointer"
+                  >
+                    Remove all
+                  </div>
+                )}
               </div>
-              {historyList?.map(history => (
-                <div className="px-[32px] py-[16px] text-sm text-slate-800 hover:cursor-pointer hover:bg-slate-100">
-                  {history}
+              {!!history.length && (
+                <div className="max-h-[500px] overflow-auto">
+                  {history?.map(history => (
+                    <div
+                      onClick={onClickWord.bind(null, history)}
+                      className=" border-b border-solid border-gray-200 px-[32px]  py-[16px] text-sm text-slate-800 last:border-b-0 hover:cursor-pointer hover:bg-slate-100 hover:last:rounded-b-lg"
+                    >
+                      {history}
+                    </div>
+                  ))}
                 </div>
-              ))}
+              )}
             </div>
           )}
         </div>
